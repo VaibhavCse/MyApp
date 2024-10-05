95f60d8cf1b554daf0f5054b0361a98088351f3f
Vaibhav Chaudhary <vaibhav77324@gmail.com>
2024-10-05 23:12:42 +0530

    package updates

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
